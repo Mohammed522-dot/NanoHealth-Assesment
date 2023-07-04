abe5d2315193a825347de10e0ae5d074db710b60
moahamed88 <40872329+moahamed88@users.noreply.github.com>
2023-07-04 05:30:38 +0300

    commite v1

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Nano Health',
         debugShowCheckedModeBanner: false,
-        home: const RootApp()
+        home: const LoginPage()
       ),
     );
   }
